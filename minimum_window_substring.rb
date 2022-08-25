3ef7cdc77f9b282759559dd693c386b3ece7b116
DMutuba <daniel@doublegdp.com>
2022-08-25 10:41:29 +0300

    minimum_window_substring window substring ruby solution

--- minimum_window_substring.rb
+++ minimum_window_substring.rb
@@ -1,39 +1,61 @@
 def minimum_window_substring(search_string, string_to_be_found)
-  return '' if t == ''
+  return '' if string_to_be_found == ''
 
-  count_t = {}
-  window = {}
+  string_to_be_found_hash = {}
+  search_string_window_hash = {}
 
-  string_to_be_found.each_char do |c|
-    count_t[c] = 1 + count_t.fetch(c, 0)
+  # build the string_to_be_found hash
+  string_to_be_found.each_char do |char|
+    string_to_be_found_hash[char] = 1 + string_to_be_found_hash.fetch(char, 0)
   end
 
   have = 0
-  need = count_t.length
+  need = string_to_be_found_hash.length
 
   result = [-1, -1]
-  result_length = Float::INFINITY
-  left = 0
+  result_length = Float::INFINITY # any value will be less than this
+  left_pointer = 0
   search_string.each_char.with_index do |char, index|
-    window[char] = 1 + window.fetch(char, 0)
-    have += 1 if count_t.key?(:char) && window[char] == count_t[char]
+    # build the window hash with mappings ofr key, value based on search string
+    search_string_window_hash[char] = 1 + search_string_window_hash.fetch(char, 0)
 
+    # increase the haves on condition the current char, is a key in
+    # string_to_be_found_hash and we have met the condition
+    # for character count in the string_to_be_found_hash, i.e we need 1 A in string to be found and
+    # we have 1 A from the search_string_window_hash
+    have += 1 if string_to_be_found_hash.key?(char) &&
+                 search_string_window_hash[char] == string_to_be_found_hash[char]
+
+    # start
     while have == need
-        if( index- left + 1 ) < result_length
-            result = [left, index]
-            result_length = index- left + 1 
-        end
+      if (index - left_pointer + 1) < result_length
+        result = [left_pointer, index] # update the result left and right pointers
+        result_length = index - left_pointer + 1 # shrink the current window
+      end
 
-        # pop from the left
-        window[search_string[l]] -= 1
+      # pop from the left
+      search_string_window_hash[search_string[left_pointer]] -= 1
 
-        if count_t.key?(search_string[left]) && window[search_string[left]] < count_t[search_string[left]]
-            have -= 1
-        end
+      if string_to_be_found_hash.key?(search_string[left_pointer]) &&
+         search_string_window_hash[search_string[left_pointer]] < string_to_be_found_hash[search_string[left_pointer]]
+        have -= 1
+      end
+      # actually move the left pointer
+      left_pointer += 1
     end
   end
 
-  left, right_index = result 
+  # pick the left and right pointers
+  left, right_pointer= result
 
-   search_string[left : right_index + 1] if result_length != Float::INFINITY else ""
+  if result_length != Float::INFINITY
+    search_string[left..right_pointer + 1]
+  else
+    ''
+  end
 end
+
+s = 'ADOBECODEBANC'
+t = 'ABC'
+
+p minimum_window_substring(s, t)
--- top_k_most_frequent_element.rb
+++ top_k_most_frequent_element.rb
@@ -2,27 +2,24 @@ def top_k_most_frequent_elements(arr, k)
   hash = {}
 
   freq_array = []
-  (arr.length + 1).times { |n| freq_array << []}
+  (arr.length + 1).times { |_n| freq_array << [] }
 
   arr.each do |n|
     hash[n] = 1 + hash.fetch(n, 0)
   end
 
-   hash.each do |key, value|
-     freq_array[value] << key
-   end
+  hash.each do |key, value|
+    freq_array[value] << key
+  end
 
   result = []
 
   freq_array.reverse.each do |element|
     element.each do |x|
       result << x
-      if result.length == k
-        p result
-      end
+      p result if result.length == k
     end
-
   end
 end
 
-topKMostFrequentElements([12,12,34,56,34,12,47,40,40,35], 3)
+topKMostFrequentElements([12, 12, 34, 56, 34, 12, 47, 40, 40, 35], 3)
