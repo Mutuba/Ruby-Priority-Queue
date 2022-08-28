4c43e7f529ff34c5b183a14d02722f8d7f15fbc1
DMutuba <daniel@doublegdp.com>
2022-08-28 16:27:03 +0300

    two sum sorted

---
+++ two_sum_sorted.rb
@@ -0,0 +1,21 @@
+def two_sum_sorted(input_array, target)
+  left_pointer = 0
+  right_pointer = input_array.length - 1
+  result = []
+  while left_pointer < right_pointer
+    current_sum = input_array[left_pointer] + input_array[right_pointer]
+    if current_sum < target
+      left_pointer += 1
+    elsif current_sum > target
+      right_pointer -= 1
+    else
+      result = [right_pointer + 1, left_pointer + 1]
+      break
+    end
+  end
+  result
+end
+
+sample_array = [1, 3, 4, 5, 7, 11]
+
+p two_sum_sorted(sample_array, 9)
