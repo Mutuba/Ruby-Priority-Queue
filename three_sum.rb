ac42a3fa96fea24f024337e9bb189369f918bb9a
DMutuba <daniel@doublegdp.com>
2022-08-28 12:47:32 +0300

    three sum no duplicates

---
+++ three_sum.rb
@@ -0,0 +1,23 @@
+def three_sum(input_array, target) 
+  hash = {}
+
+  # works if no duplicates
+  input_array.each.with_index do |elem, index|
+    hash[elem] = index
+  end
+  result = []
+  input_array.each.with_index do |element, index|
+    difference = target - element
+    (index + 1..input_array.length - 1).each.with_index do |element, inner_index|
+      num = difference - element
+      result = [hash[num], inner_index, index] if hash.key?(num) && hash[num] != inner_index
+    end
+  end
+
+  result
+end
+
+sample_array = [3, 4, 2, 5, 6, 8, 9, 10]
+
+# sample_array = [-1, 0, 1, 2, -1, -4]
+p three_sum(sample_array, 9)
