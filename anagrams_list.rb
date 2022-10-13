108a95137713ccc2348da461d65d45b0dfd24c6a
Mutuba <danielmutubait@gmail.com>
2022-10-13 10:32:04 +0300

    revisit algorithms

--- anagrams_list.rb
+++ anagrams_list.rb
@@ -1,24 +1,24 @@
-# def anagram_list(word)
-#   words = File.foreach('/usr/share/dict/words').map(&:chomp)
+def anagram_list(word)
+  words = File.foreach('/usr/share/dict/words').map(&:chomp)
 
-#   words_hash = words.each_with_object(Hash.new([])) do |word, hash|
-#     hash[word.chars.sort.join] += [word]
-#   end
-
-#   words_hash[word.chars.sort.join]
-# end
-
-# p anagram_list('suacal')
-
-def generate_anagrams(word)
-  words_list = File.foreach('/usr/share/dict/words').map(&:chomp)
-  dict = Hash.new { |h, k| h[k] = [] }
-
-  words_list.each do |word|
-    dict[word.chars.sort.join] << word
+  words_hash = words.each_with_object(Hash.new { |h, k| h[k] = [] }) do |word, hash|
+    hash[word.chars.sort.join] << word
   end
 
-  dict[word.chars.sort.join]
+  words_hash[word.chars.sort.join]
 end
 
-p generate_anagrams('saucal')
+p anagram_list('suacal')
+
+# def generate_anagrams(word)
+#   words_list = File.foreach('/usr/share/dict/words').map(&:chomp)
+#   dict = Hash.new { |h, k| h[k] = [] }
+
+#   words_list.each do |word|
+#     dict[word.chars.sort.join] << word
+#   end
+
+#   dict[word.chars.sort.join]
+# end
+
+# p generate_anagrams('saucal')
--- remove_duplicates_from_sorted_array.rb
+++ remove_duplicates_from_sorted_array.rb
@@ -1,7 +1,7 @@
 def remove_duplicates_from_sorted_array(input_array)
   # taking advantage of a sorted array
   # have two pointers both running from the biggining
-  # of the array i.e slow and fast pointers at indeces 0 and 1 respectively
+  # of the array i.e slow and fast pointers at indices 0 and 1 respectively
   # whenever you encounter a unique element at fast index copy it to slow index
   # else for duplicate (elem[slow] == elem[fast]), move the fast pointer
 
@@ -19,7 +19,8 @@ def remove_duplicates_from_sorted_array(input_array)
     fast += 1
   end
 
-  slow + 1
+  slow + 1 # common to return the size
+  input_array[slow..slow+1].sort
 end
 
 sample_array = [2, 2, 2, 2, 2, 3, 3, 3]
