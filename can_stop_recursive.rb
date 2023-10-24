8cd1ff50d2b461dc675a7092116585503861b56a
Mutuba <danielmutubait@gmail.com>
2023-10-24 17:57:51 +0300

    implement can_stop_iterative

--- can_stop_recursive.rb
+++ can_stop_recursive.rb
@@ -1,6 +1,6 @@
 def can_stop_recursive(runway, speed, position = 0)
 
-  return false
+  return true if speed == 0
 
   # speed cannot be negative, we cannot land on a spike and we need to be within array bounds
   if speed < 0 || runway[position] == false || position < 0 || position >= runway.length
@@ -8,6 +8,7 @@ def can_stop_recursive(runway, speed, position = 0)
   end
 
 [speed, speed -1, speed + 1].each do |speed|
+  next if speed < 0 
   if can_stop_recursive(runway, speed, position + speed)
     return true
   end
