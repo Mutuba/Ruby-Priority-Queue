9ec7957cf31a54de14cc5166fab993e0249f130f
Mutuba <danielmutubait@gmail.com>
2023-10-26 17:39:16 +0300

    implement can_stop_iterative

--- longest_substring_with_k_distinct_characters.rb
+++ longest_substring_with_k_distinct_characters.rb
@@ -1,37 +1,73 @@
+# def longest_substring_with_k_distinct_character(input_string, k)
+#   max_string = ''
+#   current_max_substring = ''
+#   hash = {}
+
+#   return nil if input_string.length == 0 || k <= 0 || k > input_string.length || input_string == ''
+
+#   # complexity is O(k) for inner loop
+#   # and O(n) for the outer loop hence O(n)
+#   # time complexity is O(n) and space complexity of 0(n) for creating a hashmap
+
+#   (0..(input_string.length - 1)).each do |index|
+#     count = 0
+#     right = index
+
+#     while count <= k && right < input_string.length
+#       # if element not in the hash, then it is unique, add to the hash, increase the count
+#       # else not unique, just increase the window
+#       if hash.key?(input_string[right])
+#         right += 1
+#         next
+#       end
+#       # element is unique add to the hash, increase the count
+#       hash[input_string[right]] = 1
+#       right += 1
+#       count += 1
+#     end
+#     current_max_substring = input_string[index...right - 1]
+#     max_string = current_max_substring if current_max_substring.length > max_string.length
+#     hash.clear
+#   end
+
+#   max_string
+# end
+
+# sample_string = 'bccbababcedd'
+
+# p longest_substring_with_k_distinct_character(sample_string, 2)
+
+
 def longest_substring_with_k_distinct_character(input_string, k)
+  # Steps
+  # 1. hash to keep track of unique xters count
+  # 2. loop while still within input_string length
+  # 3. Create different windows on input_string and compare to come up with max
+  # 4. Have index, that is moves from left to right as well as right that starts when equal to index
+  
   max_string = ''
   current_max_substring = ''
-  hash = {}
-
-  return nil if input_string.length == 0 || k <= 0 || k > input_string.length || input_string == ''
-
-  # complexity is O(k) for inner loop
-  # and O(n) for the outer loop hence O(n)
-  # time complexity is O(n) and space complexity of 0(n) for creating a hashmap
-
   (0..(input_string.length - 1)).each do |index|
-    count = 0
+    hash = {}
+    count =  0
     right = index
-
     while count <= k && right < input_string.length
-      # if element not in the hash, then it is unique, add to the hash, increase the count
-      # else not unique, just increase the window
       if hash.key?(input_string[right])
         right += 1
         next
       end
-      # element is unique add to the hash, increase the count
+
       hash[input_string[right]] = 1
       right += 1
       count += 1
     end
-    current_max_substring = input_string[index...right - 1]
-    max_string = current_max_substring if current_max_substring.length > max_string.length
-    hash.clear
-  end
 
+    current_max_substring = input_string[index...right-1]
+    max_string = current_max_substring if current_max_substring.length > max_string.length
+  end
   max_string
 end
+
 
 sample_string = 'bccbababcedd'
 
