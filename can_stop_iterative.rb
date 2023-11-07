206c1c5dbe1cae0d37849746b1b11d33e29163fe
Mutuba <danielmutubait@gmail.com>
2023-11-07 17:47:23 +0300

    fix typos

--- can_stop_iterative.rb
+++ can_stop_iterative.rb
@@ -1,6 +1,6 @@
 def can_stop_iterative(runway, initial_speed, initial_position = 0)
   return false if initial_speed < 0
-  
+
   return true if initial_speed == 0
 
   queue = [[initial_position, initial_speed]] # Initialize a queue with initial position and speed
@@ -13,6 +13,7 @@ def can_stop_iterative(runway, initial_speed, initial_position = 0)
     return true if speed == 0
 
     # Try three possible speeds: current speed, current speed - 1, current speed + 1
+    
     [speed, speed - 1, speed + 1].each do |new_speed|
       next if new_speed < 0 # Skip negative speeds
 
