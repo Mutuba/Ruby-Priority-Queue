f3212db46e1f338f48d88e64ee66a5ef6eed7ce1
Mutuba <danielmutubait@gmail.com>
2023-12-08 19:26:43 +0300

    solve unique visits problem

--- unique_visits_problem.rb
+++ unique_visits_problem.rb
@@ -69,3 +69,44 @@ def parse_file(file_path)
 
   entries
 end
+
+
+require 'set'
+
+def loyal_customers(log_file_path_day1, log_file_path_day2)
+  log_day1 = parse_file(log_file_path_day1)
+  log_day2 = parse_file(log_file_path_day2)
+
+  customer_pages = Hash.new { |hash, key| hash[key] = Set.new }
+
+  # Process log_day1
+  log_day1.each do |entry|
+    customer_id = entry[:customer_id]
+    page_id = entry[:page_id]
+
+    customer_pages[customer_id].add(page_id)
+  end
+
+  # Process log_day2
+  common_customers = Set.new
+  log_day2.each do |entry|
+    customer_id = entry[:customer_id]
+    page_id = entry[:page_id]
+
+    # check if customer came day 1
+    if customer_pages.key?(customer_id)
+      customer_pages[customer_id].add(page_id)
+
+      # If the customer has at least two unique pages, mark as common customer
+      common_customers.add(customer_id) if customer_pages[customer_id].size >= 2
+    end
+  end
+
+  common_customers.to_a
+end
+
+# Example Usage
+log_file_path_day1 = 'path/to/log/day1.txt'
+log_file_path_day2 = 'path/to/log/day2.txt'
+
+puts loyal_customers(log_file_path_day1, log_file_path_day2)
