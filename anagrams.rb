64df11d1b36519603bc46268c8fc3fc422beb7b5
Mutuba <danielmutubait@gmail.com>
2024-03-03 11:44:31 +0300

    Refactor analgrams solution

--- anagrams.rb
+++ anagrams.rb
@@ -18,22 +18,9 @@
 
 
 def is_anagram(first_word, second_word)
-  return false unless first_word || second_word
+  return false unless first_word & second_word
   
   return false if first_word_sorted.length != second_word.length
 
-  first_word_sorted = first.chars.sort.join
-  second_word_sorted = second_word.chars.sort.join
-
-  position = 0
-  character_matches = true
-  while position < first_word_sorted.length && character_matches
-    
-    if first_word_sorted[position] == second_word_sorted[position]
-      position += 1
-    else
-      character_matches = false
-    end
-  end
-  character_matches
+  first.chars.sort.join == second_word.chars.sort.join
 end
