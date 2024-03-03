0afca910a11ad00058fa10f48ce344c73d305526
Mutuba <danielmutubait@gmail.com>
2024-03-03 16:13:40 +0300

    Implement longest substring with atleast k distinct characters

--- longest_substring_with_atleast_k_distinct_characters.rb
+++ longest_substring_with_atleast_k_distinct_characters.rb
@@ -4,11 +4,11 @@ def longest_substring_with_atleast_k_distinct_character(input_string, k)
     hash = Hash.new(0)
     right = index
     while right < input_string.length
-      hash[input_string[right]] = 1
+      hash[input_string[right]] += 1
       right += 1
     end
 
-    if hash.keys.length >= k
+    if hash.length >= k
       current_max_substring = input_string[index...right]
       max_string = current_max_substring if current_max_substring.length > max_string.length
     end
