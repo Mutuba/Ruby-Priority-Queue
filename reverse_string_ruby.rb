e578e66683cdaffbe1ac83c91d24feec403022f3
Mutuba <danielmutubait@gmail.com>
2024-06-26 12:09:37 +0300

    Improve 2 sum using hash problem

--- reverse_string_ruby.rb
+++ reverse_string_ruby.rb
@@ -1,5 +1,5 @@
 def reverse_string(input_string)
-  # need to be clear whether whether it is a string of characters
+  # need to be clear whether it is a string of characters
   # or a string of words depending you call split method appropriately
   # say it is a string of characters
   character_array = input_string.chars
