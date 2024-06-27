b3f2b95bdf2ea3c3e7cecef46a30540b67051d89
Mutuba <danielmutubait@gmail.com>
2024-06-27 18:48:38 +0300

    Clean up

--- can_stop_iterative.rb
+++ can_stop_iterative.rb
@@ -25,31 +25,3 @@ def can_stop_iterative(runway, initial_speed, initial_position = 0)
   false
 end
 
-
-
-
-def can_stop_iterative(runway, initial_position=0, initial_speed)
-    return false unless initial_speed > 0
-    return true if initial_speed == 0
-
-    # initialize a queue with the initial position and speed
-    # we can create an actual queue
-    my_array  =  [[initial_position, initial_speed]]
-
-    while my_array.any?
-      # get the last speed and position
-      position, speed = my_array.pop
-
-      return true if speed == 0
-      # invalid position
-      next if position < 0 || position >= runway.length
-
-      [speed -1 , speed , speed + 1].each do |new_speed|
-        next if new_speed < 0
-        # my_array << [position + new_speed, new_speed]
-        my_array.push [position + new_speed, new_speed]
-
-      end
-    end
-    false
-end
