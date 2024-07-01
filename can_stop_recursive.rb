b1dd8ca2b5fb0d9a2902a27b821f224c47928e9c
Mutuba <danielmutubait@gmail.com>
2024-07-01 10:18:41 +0300

    DAS clean up

--- can_stop_recursive.rb
+++ can_stop_recursive.rb
@@ -3,7 +3,7 @@ def can_stop_recursive(runway, speed, position = 0)
   return true if speed == 0
 
   # speed cannot be negative, we cannot land on a spike and we need to be within array bounds
-  if speed < 0 || !runway[position] || position < 0 || position >= runway.length
+  if position < 0 || position >= runway.length
     return false
   end
 
