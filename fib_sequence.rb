f9b655b74ada462eae7e2f217e1df11123603b3f
Mutuba <danielmutubait@gmail.com>
2024-07-03 15:53:32 +0300

    Add fibonacci sequence

---
+++ fib_sequence.rb
@@ -0,0 +1,17 @@
+def fibonacci_recursive(n)
+  return n if n < 2
+
+  fib0, fib1 = 0,1
+  (2..n).each do |i|
+    fib0, fib1 = fib1,  fib0 + fib1
+  end
+  fib1
+  # fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+end
+
+
+# fibonachi_sequence(n-1) + fibonachi_sequence(n-2)
+puts fibonacci_recursive(0)  # Output: 0
+puts fibonacci_recursive(1)  # Output: 1
+puts fibonacci_recursive(5)  # Output: 5
+puts fibonacci_recursive(10) # Output: 55
