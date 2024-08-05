5ced899375db36eb8e44d57c40e6a9a81e988428
Mutuba <danielmutubait@gmail.com>
2024-08-05 18:01:51 +0300

    Fix queue problem

--- can_stop_iterative.rb
+++ can_stop_iterative.rb
@@ -3,10 +3,10 @@ def can_stop_iterative(runway, speed, position = 0)
 
   return true if speed == 0
 
-  array_queue = [[position, speed]] # Initialize a queue with initial position and speed
+  queue = [[position, speed]] # Initialize a queue with initial position and speed
 
-  while array_queue.any?
-    position, speed = array_queue.shift # Dequeue a position and speed
+  while queue.any?
+    position, speed = queue.shift # Dequeue a position and speed
 
     next if position >= runway.length # Skip invalid positions
 
@@ -17,7 +17,7 @@ def can_stop_iterative(runway, speed, position = 0)
     [speed, speed - 1, speed + 1].each do |new_speed|
       next if new_speed < 0 # Skip negative speeds
       # Enqueue the new position and speed
-      array_queue.push([position + new_speed, new_speed])
+      queue.push([position + new_speed, new_speed])
     end
   end
 
