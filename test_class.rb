9beb25a1fba0c173d8357b050d1dfe65a3c2a3d6
Mutuba <danielmutubait@gmail.com>
2024-08-05 10:05:16 +0300

    Fix error handling module

--- test_class.rb
+++ test_class.rb
@@ -36,3 +36,45 @@ class Test
 end
 
 p Test.set_association
+
+# Adapter pattern in play using Ruby
+
+# Existing e-commerce platform interface
+
+class ECommercePlatform
+  def process_payment(amount)
+    raise NotImplementedError, "This method should be implemented by concrete subclasses"
+  end
+end
+
+# New payment gateway interface
+
+class NewPaymentGateway
+  def execute_payment(amount)
+    puts "Executing payment of $#{amount} via the new payment gateway."
+  end
+end
+
+# Adapter class
+
+class PaymentGatewayAdapter < ECommercePlatform
+  def initialize(payment_gateway)
+    @payment_gateway = payment_gateway
+  end
+
+  def process_payment(amount)
+    @payment_gateway.execute_payment(amount)
+  end
+end
+
+# Usage example
+ecommerce_platform = ECommercePlatform.new
+
+# Integration of the new payment gateway via the adapter
+new_payment_gateway = NewPaymentGateway.new
+adapter = PaymentGatewayAdapter.new(new_payment_gateway)
+
+amount = 100.0
+
+# Invoke the adapter
+adapter.process_payment(amount)  #=> Executing payment of $100.0 via the new payment gateway
