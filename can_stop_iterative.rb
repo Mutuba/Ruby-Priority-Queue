cef1e2a24b891ed3eaac9dff1d4725b7a91c25fd
Mutuba <danielmutubait@gmail.com>
2024-08-05 17:58:32 +0300

    Fix binary gap solution

--- can_stop_iterative.rb
+++ can_stop_iterative.rb
@@ -1,18 +1,18 @@
-def can_stop_iterative(runway, initial_speed, initial_position = 0)
-  return false if initial_speed < 0
+def can_stop_iterative(runway, speed, position = 0)
+  return false if speed < 0
 
-  return true if initial_speed == 0
+  return true if speed == 0
 
-  array_queue = [[initial_position, initial_speed]] # Initialize a queue with initial position and speed
+  array_queue = [[position, speed]] # Initialize a queue with initial position and speed
 
   while array_queue.any?
     position, speed = array_queue.shift # Dequeue a position and speed
 
-    next if position < 0 || position >= runway.length # Skip invalid positions
+    next if position >= runway.length # Skip invalid positions
 
     return true if speed == 0
 
-    # Try three possible speeds: current speed, current speed - 1, current speed + 1
+    # Try three possible speeds: speed, speed - 1, speed + 1
     
     [speed, speed - 1, speed + 1].each do |new_speed|
       next if new_speed < 0 # Skip negative speeds
